bfe0857df50c7d8e0ea6154b860a3e29f6e2025e
Leonpsilv <leonardo.pin.s2002@gmail.com>
2022-06-06 22:44:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const adicionarElemento = (event) => {
     //Captura o valor
     const valor = event.target.elements[0].value;
     console.log(valor);
+    event.target.elements[0].value = ""; // para limpar o campo
 }
     const elementoForm = document.querySelector("form");
     elementoForm.addEventListener('submit', adicionarElemento);
