91cd0b9a044b3a27238f0d1e9aa2be9c32bca61e
Leonpsilv <leonardo.pin.s2002@gmail.com>
2022-06-07 16:48:05 -0400

    submit modifica o formulario

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
+const listaDeItems = [];
+let idItemContador = 0;
+const conteudoLista = document.querySelector('.conteudoLista');
+
+const criarElemento = (elementoItem) => {
+    conteudoLista.innerHTML = `
+        <h3 class="tituloLista">Sua lista :)</h3>
+        <ul class="listaNova">
+        </ul>
+    `;
+    const listaNova = document.querySelector('.listaNova');
+    listaNova.appendChild(elementoItem);
+
+}
+
+const criarArrayLista = (valor) => {
+    const idItem = idItemContador;
+    listaDeItems[idItemContador] = valor; // armazenando o valor digitado em uma array na posicao idItemContador
+    const elementoItem = document.createElement('li');
+    elementoItem.innerText = listaDeItems [idItemContador];
+    elementoItem.setAttribute('id', idItemContador);
+
+    idItemContador++;
+    criarElemento(elementoItem);
+}
+
 const adicionarElemento = (event) => {
     event.preventDefault();
 
     //Captura o valor
     const valor = event.target.elements[0].value;
-    console.log(valor);
     event.target.elements[0].value = ""; // para limpar o campo
+
+    criarArrayLista(valor);
 }
+
     const elementoForm = document.querySelector("form");
-    elementoForm.addEventListener('submit', adicionarElemento);
+    elementoForm.addEventListener('submit', adicionarElemento); // chamará a função adicionarElemento quando ocorrer submit
 
 
 
