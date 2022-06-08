2b8c4bba7df01b9e5409e8c31a0419032d165850
Leonpsilv <leonardo.pin.s2002@gmail.com>
2022-06-08 14:56:55 -0400

    itens da lista aparecem com botão de apagar

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const listaDeItems = [];
 let idItemContador = 0;
 const lista = document.querySelector('.lista');
 
+const apagarElemento = () => {
+    
+}
+
+const itemApagarElemento = (idItemContador) => {
+    const itemDelete = document.createElement('input');
+    itemDelete.value = "X"; 
+    itemDelete.setAttribute('type', 'button');
+    itemDelete.setAttribute('class', 'removedor')
+    itemDelete.addEventListener('click', () => apagarElemento(idItemContador))
+
+    return itemDelete;
+}
 
 const criarElemento = (valor) => {
     listaDeItems[idItemContador] = valor; // armazenando o valor digitado em uma array na posicao idItemContador
@@ -10,8 +23,8 @@ const criarElemento = (valor) => {
     elementoItem.setAttribute('id', idItemContador);
     // após criar o elemento, vamos criar o botão de apagá-lo
 
-    
-
+    const apagarItem = itemApagarElemento(idItemContador);
+    elementoItem.appendChild(apagarItem);
 
     lista.appendChild(elementoItem);
  
