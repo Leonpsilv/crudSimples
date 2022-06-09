b06c9c76dc766b4f8568a4b4454d3ea9c579ecc8
Leonpsilv <leonardo.pin.s2002@gmail.com>
2022-06-08 23:12:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const lista = document.querySelector('.lista');
 
 const apagarElemento = (idItemContador) => {
     console.log('apagar :' + idItemContador);
+    console.log(listaDeItems);
 }
 
 const itemApagarElemento = (idItemContador) => {
@@ -17,33 +18,43 @@ const itemApagarElemento = (idItemContador) => {
     return itemDelete;
 }
 const limpaLista = () => {
-    //console.log('listaLimpa');
+    conteudoLista.innerHTML=`
+    <h3 class="tituloLista">Sua lista :)</h3>
+    <ul class="listaNova">
+        
+    </ul>
+    `;
+    
+}
+const criarElementoTeste = (valor) => {
+    limpaLista();
+    listaDeItems[idItemContador] = valor; // armazenando o valor digitado em uma array na posicao idItem
+    //let 
+    for(let i=0; i <= idItemContador; i++){
+        const elementoItem = document.createElement('li');
+        elementoItem.innerText = elemento;
+        elementoItem.setAttribute('id', listaDeItems.length);
+    }
 }
 
 const criarElemento = (valor) => {
     limpaLista();
-    //const idItem = idItemContador;
-    listaDeItems[idItemContador] = valor; // armazenando o valor digitado em uma array na posicao idItemContador
-
+    //let idItem = idItemContador;
+    listaDeItems[idItemContador] = valor; // armazenando o valor digitado em uma array na posicao idItem
+    
     listaDeItems.forEach((elemento) => {
+        
         const elementoItem = document.createElement('li');
         elementoItem.innerText = elemento;
         elementoItem.setAttribute('id', idItemContador);
             // após criar o elemento, vamos criar o botão de apagá-lo:
         const apagarItem = itemApagarElemento(idItemContador);
         elementoItem.appendChild(apagarItem);
-        
-        lista.appendChild(elementoItem);
+        const listaNova = document.querySelector('.listaNova');
+        listaNova.appendChild(elementoItem);
         //console.log(idItemContador);
         idItemContador++;
     });
-    //idItemContador++;
-}
-const verificaLista = () => {
-    if(idItemContador !== 0) {
-        document.querySelector('.TextoListaVazia').innerText = '';
-    }
-    
 }
 
 const adicionarElemento = (event) => {
@@ -53,10 +64,8 @@ const adicionarElemento = (event) => {
     const valor = event.target.elements[0].value;
     event.target.elements[0].value = ""; // para limpar o campo
 
-    criarElemento(valor);
-    //console.log(listaDeItems);///////////////////
-
-    verificaLista();
+    //criarElemento(valor);
+    criarElementoTeste(valor);
 }
 
     const elementoForm = document.querySelector("form");
